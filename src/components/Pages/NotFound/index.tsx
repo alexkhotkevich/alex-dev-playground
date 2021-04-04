aef295ef965e536f31d317abe50a2fe2d36fcd12
alexanderkhotkevich <alexanderkhotkevich@gmail.com>
2021-04-04 14:37:25 +0300

    +About +404

---
+++ src/components/Pages/NotFound/index.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
+export const NotFound = () => {
+  return <Typography variant='h1'>404</Typography>
+}
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
-import { Main } from 'components/Main'
+import { About, Main, NotFound } from 'components/Pages'
+
 export const Router = () => {
   return (
     <Switch>
       <Route exact strict sensitive path='/'>
         <Main />
+      </Route>
+      <Route exact strict sensitive path='/about'>
+        <About />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   )
